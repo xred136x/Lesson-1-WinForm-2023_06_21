e08d86f4f4871a8481621c17d8c79356cc88d6ee
С - 3 < - 3@DESKTOP-E0FTS21>
2023-06-21 22:27:49 +0300

    Добавьте файлы проекта.

---
+++ Lesson 1 WinForm 2023_06_21/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Lesson_1_WinForm_2023_06_21
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelOutput = new System.Windows.Forms.Label();
+            this.textBoxInput = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.Viewer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // labelOutput
+            // 
+            this.labelOutput.AutoSize = true;
+            this.labelOutput.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.labelOutput.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelOutput.Location = new System.Drawing.Point(12, 147);
+            this.labelOutput.Name = "labelOutput";
+            this.labelOutput.Size = new System.Drawing.Size(159, 33);
+            this.labelOutput.TabIndex = 0;
+            this.labelOutput.Text = "LabelOutput";
+            // 
+            // textBoxInput
+            // 
+            this.textBoxInput.BackColor = System.Drawing.SystemColors.Window;
+            this.textBoxInput.Font = new System.Drawing.Font("Tahoma", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBoxInput.ForeColor = System.Drawing.Color.Black;
+            this.textBoxInput.Location = new System.Drawing.Point(7, 67);
+            this.textBoxInput.Name = "textBoxInput";
+            this.textBoxInput.Size = new System.Drawing.Size(862, 40);
+            this.textBoxInput.TabIndex = 1;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnOpen.Location = new System.Drawing.Point(313, 312);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(107, 33);
+            this.btnOpen.TabIndex = 2;
+            this.btnOpen.Text = "Открыть";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.MouseClick += new System.Windows.Forms.MouseEventHandler(this.btnOpen_MouseClick);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnSave.Location = new System.Drawing.Point(520, 312);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(110, 36);
+            this.btnSave.TabIndex = 2;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // Viewer
+            // 
+            this.Viewer.AutoWordSelection = true;
+            this.Viewer.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Viewer.Location = new System.Drawing.Point(12, 183);
+            this.Viewer.Name = "Viewer";
+            this.Viewer.Size = new System.Drawing.Size(857, 96);
+            this.Viewer.TabIndex = 3;
+            this.Viewer.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(881, 357);
+            this.Controls.Add(this.Viewer);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.textBoxInput);
+            this.Controls.Add(this.labelOutput);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.SystemColors.InfoText;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelOutput;
+        private System.Windows.Forms.TextBox textBoxInput;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox Viewer;
+    }
+}
+
---
+++ Lesson 1 WinForm 2023_06_21/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lesson_1_WinForm_2023_06_21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            var color = new Color();
+            color = Color.FromArgb(255,220,0,145);
+            
+            labelOutput.ForeColor = color;
+            
+            try
+            {
+                var number = Convert.ToInt32(textBoxInput.Text);
+                var res = Math.Pow(number,2).ToString();
+                labelOutput.Text = res;
+            }
+            catch
+            {
+                var message = "Не удалось" + " преобразовать в число";
+                MessageBox.Show(message);
+                labelOutput.Text = message;
+            }
+           
+        }
+
+        private void btnOpen_MouseClick(object sender, MouseEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();            
+            ofd.InitialDirectory = Environment.CurrentDirectory;
+            ofd.Filter = "Текстовые (шаблон*.txt)|*.txt"+"|Исходник (шаблон*.cs)|*.cs"+
+                "|Все (шаблон*.*)|*.*";
+            var result = ofd.ShowDialog(this);
+            if(result == DialogResult.OK)
+            {
+                using (StreamReader sr = new StreamReader(ofd.FileName))
+                {
+                    Viewer.Text = sr.ReadToEnd();
+                }
+               
+            }
+        }
+
+        
+    }
+}
